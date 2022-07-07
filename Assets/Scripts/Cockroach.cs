e373460a5b29e118a976492abd8e425c3a743dd8
vvr <vover@mail.ru>
2022-07-07 11:30:58 +0300

    префаб таракана

---
+++ Assets/Scripts/Cockroach.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Cockroach : MonoBehaviour
+{
+    private Animation anim;
+    [SerializeField] private GameObject cockroach;
+    [SerializeField] private GameObject deadCockroach;
+    // private float currentTime = 0f;
+    // private float maxTime = 3f;
+    // private bool isKilled;
+
+    private void Awake()
+    {
+        deadCockroach.SetActive(false);
+        anim = GetComponent<Animation>();
+    }
+    
+    
+    // void Update()
+    // {
+    //     currentTime += Time.deltaTime;
+    //     if (currentTime >= maxTime && !isKilled)
+    //     {
+    //         KillCockroach();
+    //         isKilled = true;
+    //     }
+    // }
+
+    public void KillCockroach()
+    {
+        deadCockroach.transform.position = cockroach.transform.position;
+        deadCockroach.SetActive(true);
+        anim.enabled = false;
+        cockroach.SetActive(false);
+    }
+}

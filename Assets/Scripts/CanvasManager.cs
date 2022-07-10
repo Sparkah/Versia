8443fd862acbe6e7b6dbc9c7b59805a39b90ff5e
Tim <timopro16@gmail.com>
2022-07-10 18:42:43 +0300

    zagoni mastered

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -16,6 +16,13 @@ public class CanvasManager : MonoBehaviour
     private bool canAppear = true;
     [SerializeField] private Image scaryFadeMain;
     private float scareFadeMultiplier;
+    public Sprite[] m_MainTexture;
+    public Sprite[] m_AdditionalAlpha;
+    private int textureCount = 0;
+    private int textureAdditionalCount = 0;
+    private int textureAmount;
+    private int additionalTextureAmount;
+    [SerializeField] private Image scareFadeAdditionalAlpha;
 
     void Start()
     {
@@ -25,6 +32,46 @@ public class CanvasManager : MonoBehaviour
         timeUIToAppear = sceneSettings.timeUIToAppear;
         timeToNextScene = sceneSettings.TimeToNextScene;
         time = 0;
+        textureAmount = m_MainTexture.Length;
+        additionalTextureAmount = m_AdditionalAlpha.Length;
+        Debug.Log(m_AdditionalAlpha.Length);
+        StartCoroutine(SetMaterialTexture());
+        StartCoroutine(SetAdditionalAlphaTexture());
+    }
+
+
+    // Use this for initialization
+    IEnumerator SetMaterialTexture()
+    {
+        yield return new WaitForSeconds(0.2f);
+        //15-30 = success
+        if (textureCount < textureAmount - 1)
+        {
+            scaryFadeMain.sprite= m_MainTexture[textureCount];
+            textureCount++;
+
+            StartCoroutine(SetMaterialTexture());
+        }
+        else
+        {
+            StartCoroutine(SetMaterialTexture());
+        }
+    }
+
+    IEnumerator SetAdditionalAlphaTexture()
+    {
+        yield return new WaitForSeconds(0.2f);
+        if (textureAdditionalCount < additionalTextureAmount - 1)
+        {
+            scareFadeAdditionalAlpha.sprite = m_AdditionalAlpha[textureAdditionalCount];
+            textureAdditionalCount++;
+
+            StartCoroutine(SetAdditionalAlphaTexture());
+        }
+        else
+        {
+            StartCoroutine(SetAdditionalAlphaTexture());
+        }
     }
 
     void Update()
@@ -42,16 +89,16 @@ public class CanvasManager : MonoBehaviour
             canFade = false;
             fadeImage.DOFade(1, timeUIToAppear);
         }
-
     }
 
     private IEnumerator ScareFader()
     {
         yield return new WaitForSeconds(2f);
-        scaryFadeMain.DOFade(0.01f*scareFadeMultiplier, 2f);
+        scaryFadeMain.DOFade(scareFadeMultiplier*0.01f, 1.5f);
+        scareFadeAdditionalAlpha.DOFade(scareFadeMultiplier * 0.09f, 1.5f);
         if (scareFadeMultiplier < 12f)
         {
-            scareFadeMultiplier += 0.5f;
+            scareFadeMultiplier += 2.5f;
         }
         StartCoroutine(ScareFader());
     }
@@ -60,10 +107,30 @@ public class CanvasManager : MonoBehaviour
         if (scareFadeMultiplier > 1)
         {
             scareFadeMultiplier -= 1;
+            if (textureCount > 10)
+            {
+                textureCount -= 10;
+                textureAdditionalCount -= 20;
+            }
+            else
+            {
+                textureCount = 0;
+                textureAdditionalCount = 0;
+            }
         }
         else
         {
             scareFadeMultiplier = 0;
+            if (textureCount > 10)
+            {
+                textureCount -= 10;
+                textureAdditionalCount -= 20;
+            }
+            else
+            {
+                textureCount = 0;
+                textureAdditionalCount = 0;
+            }
         }
     }
 }

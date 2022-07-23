fee84b98775187ad30fb4934519df79055747f6d
Tim <timopro16@gmail.com>
2022-07-23 14:35:15 +0300

    require collider + scene 9 playercontroller placement

--- Assets/Trudogolik/Scripts/AudioScript.cs
+++ Assets/Trudogolik/Scripts/AudioScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class AudioScript : MonoBehaviour
 {
     private AudioSource _audioSource;

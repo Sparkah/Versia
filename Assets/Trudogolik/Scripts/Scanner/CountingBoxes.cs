cbd673f2f3ddc4883ae609248c95718ea6e7cb0b
Tim <timopro16@gmail.com>
2022-07-28 14:02:55 +0300

    scanner mastered

--- Assets/Trudogolik/Scripts/Scanner/CountingBoxes.cs
+++ Assets/Trudogolik/Scripts/Scanner/CountingBoxes.cs
@@ -6,6 +6,7 @@ namespace Trudogolik
     public class CountingBoxes : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _boxCounter;
+        [SerializeField] private TextMeshProUGUI _boxCounterLeft;
         private int _counter = 0;
 
         private void Start()
@@ -13,6 +14,7 @@ namespace Trudogolik
             if (_boxCounter == null)
                 return;
             _boxCounter.text = _counter.ToString();
+            _boxCounterLeft.text = _counter.ToString();
         }
         public void UpdateCounter()
         {
@@ -20,6 +22,7 @@ namespace Trudogolik
                 return;
             _counter++;
             _boxCounter.text = _counter.ToString();
+            _boxCounterLeft.text = _counter.ToString();
         }
         public void ResetCounter()
         {
@@ -27,6 +30,7 @@ namespace Trudogolik
                 return;
             _counter = 0;
             _boxCounter.text = _counter.ToString();
+            _boxCounterLeft.text = _counter.ToString();
         }
     }
 }

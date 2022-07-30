9d81a30d5c57ca32b54786fd88a6a9ff752be947
Wartimer <danryzhkov@mail.ru>
2022-07-30 15:16:06 +0300

    Cars
    + Veterok Car Models
    + Veterok Car sounds
    + Veterok Car Controller
    + Veterok Car Pool

---
+++ Assets/Veterok/_Root/Scripts/Game/Views/CarView.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Veterok.Views
+{
+    public class CarView : MonoBehaviour
+    {
+        [field:SerializeField] public Transform Target { get; private set; }
+
+
+        public void MoveToTargetPosition()
+        {
+            
+        }
+
+        public void SetTarget(Transform target)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Veterok/_Root/Scripts/Pools/CarPool.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Veterok.Views;
+
+namespace Veterok.Pools
+{
+    internal class CarPool<T> : PoolBase<T> where T : CarView
+    {
+        private List<CarView> _carViews;
+        
+        public CarPool(int poolCapacity, Transform root, List<CarView> carViews) : base(poolCapacity, root)
+        {
+            _carViews = carViews;
+        }
+
+        public override T GetObject()
+        {
+            T obj;
+
+            if (ObjectPool.Count == 0)
+            {
+                for (var i = 0; i <= PoolCapacity; i++)
+                {
+                    obj = Object.Instantiate(_carViews[Random.Range(0, _carViews.Count)]) as T;
+                    ReturnToPool(obj);
+                }
+            }
+
+            obj = ObjectPool.Pop();
+            obj.gameObject.SetActive(true);
+            obj.transform.SetParent(null);
+            return obj;
+        }
+
+        public override void ReturnToPool(T obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
